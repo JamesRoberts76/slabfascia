2b4c7812658a319a1d3725f69b5c94e8046fbdf4
JamesRoberts76 <james.progressivepersonnel@gmail.com>
2026-07-17 11:20:50 +0100

    Create network-config.js

---
+++ network-config.js
@@ -0,0 +1,10 @@
+window.NETWORK_CONFIG = {
+  version: '1.0.0',
+  siteId: 'back.guide',
+  sessionContext: 'back',
+  legalEntity: 'Dry Holidays SL',
+  registration: 'B67894493',
+  accentColor: '#01696f',
+  apiEndpoint: '/api/chat',
+  botMountId: 'sovereign-bot-interface'
+};

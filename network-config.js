0c682eafa67c220afcdca3e903987ea1d7528f60
JamesRoberts76 <james.progressivepersonnel@gmail.com>
2026-07-28 20:31:33 +0100

    Update network-config.js

--- network-config.js
+++ network-config.js
@@ -1,10 +1,6 @@
 window.NETWORK_CONFIG = {
-  version: '1.0.0',
-  siteId: 'back.guide',
-  sessionContext: 'back',
-  legalEntity: 'Dry Holidays SL',
-  registration: 'B67894493',
-  accentColor: '#01696f',
-  apiEndpoint: '/api/chat',
-  botMountId: 'sovereign-bot-interface'
+  siteId: 'slabfascia.guide',
+  apiEndpoint: 'https://compressed.guide/api/consult',
+  clusterMaster: 'compressed.guide',
+  sessionContext: 'fascia'
 };
